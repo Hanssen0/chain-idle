abfb68761a6bfb15c08f8ace7d18f1b8ff82c024
Hanssen <handsome0hell@gmail.com>
2024-01-20 07:28:16 +0800

    chore: vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Home } from "@/pages/Home";
 import { theme } from "./theme";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <SpeedInsights />
     <Home />
   </ChakraProvider>
 );
